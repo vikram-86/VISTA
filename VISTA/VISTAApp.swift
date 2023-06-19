a4400fe3f02fc8cb54b03d98b4520fa972e093bd
Vikram <vikram.sh86@gmail.com>
2023-06-19 12:30:51 +0200

    Initial Commit

---
+++ VISTA/VISTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VISTAApp.swift
+//  VISTA
+//
+//  Created by Vikram on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct VISTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
